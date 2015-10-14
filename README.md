15480934238aeec49e67ff64ee8f53f0bc2fc5d7
iq9891 <649076408@qq.com>
2015-10-14 09:40:05 +0800

    第一次上传

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ku-jquery.qrcode
+## 文件配置 ##
+
+1. 第一步就是在当前路径打开cmd
+2. 安装引用js，输入如下代码
+<pre>bower install</pre>
+
+## 功能 ##
+
 用插件jquery.qrcode，实现前端生成二维码
---
+++ js/js.js
@@ -0,0 +1,11 @@
+﻿$(function(){
+	var text = 'https://github.com/iq9891';
+	jQuery('#qrcode').qrcode({
+		
+		text    : text,
+		render    : "canvas",  // 'canvas' or 'table'. Default value is 'canvas'
+		background : "#ffffff",
+		foreground : "#565356"
+	});
+	$("canvas").css("height", "auto");
+});
